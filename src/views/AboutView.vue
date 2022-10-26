23440c0b7b29f8895e0379495212231f6ba5651b
JDigabri <johndigabriele@gmail.com>
2022-10-26 19:29:53 -0400

    lib changes

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,12 +1,28 @@
 <template>
-  <div class="about">
+  <div class="home">
+    <span>Empty...</span>
   </div>
 </template>
 
-<style >
-  body{
-    background-color: #2D2D2F;
-  }
-
+<style scoped>
+  .home{
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    text-align: center;
+    z-index: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    font-weight: bold;
+    color: #9b9b9b;
+    font-family: 'Inter', sans-serif;
+    font-size: 24px;
+    }
+    body{
+      background-color: #2D2D2F;
+    }
+  
 
 </style>
--- src/views/InventoryView.vue
+++ src/views/InventoryView.vue
@@ -1,13 +1,21 @@
 <template>
-    <div class="about">
+    <div class="home">
+      Hello
     </div>
-  </template>
+</template>
   
-  <style >
+  <style scoped>
+  .home{
+    position: absolute;
+    background-color:#D9D9D9;
+    top: 50%; right: 50%;
+    transform: translate(61%,-75%);
+    width: 500px;
+    height: 500px;
+
+    }
     body{
       background-color: #2D2D2F;
     }
   
-  
   </style>
-  

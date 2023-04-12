15db376f180e9d4d7ae2ac08ad16ae38ca9b7630
JDigabri <johndigabriele@gmail.com>
2023-04-12 19:04:53 -0400

    lib done

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,29 +1,61 @@
 <template>
   <div class="home">
-    <router-link to="/lib">
       <div class = "game2">
-        <p style = "text-align: center; padding-left: 25px;padding-top: 10px; ">
-          Castle <br> Crashers
+        <p style = "text-align: center; padding-left: 25px;padding-top: 10px; font-size: 14.5px;">
+          CASTLE <br> CRASHERS
         </p>
         <div style="border-left: 1px solid #9b9b9b; height: 70px; margin-left: 25px; margin-right:25px;" />
         <p style="font-size: 11px; margin-top: 10px"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac risus ac quam cursus egestas sed in orci. Duis tempor libero nulla.  Ut eu interdum felis. Fusce quis lobortis lorem, eget consequat ligula. Etiam at porta neque. Sed quis tincidunt justo. Fusce lorem nisi.</p>
         <div style="height: 10px;width: 10px; margin-left: 25px"/>
         <div class="launch">
-          <img src = "./imgs/play.svg" style="margin-left: 3px; height:25px;"/>
-          <p style="margin-top: 5px; font-size: 14px; margin-left: 4px;"> LAUNCH </p>
+          <img src = "./imgs/play.svg" style="margin-left: 5px; height:22px;"/>
+          <p style="margin-top: 15px; font-size: 14px; margin-left: 7px;"> LAUNCH </p>
         </div>
+      </div>    
+      
+
+
+    <div class = "game3">
+      <p style = "text-align: center; padding-left: 25px;padding-top: 10px;font-size: 12.5px;; ">
+        CYBERPUNK <br> 2077
+      </p>
+      <div style="border-left: 1px solid #9b9b9b; height: 70px; margin-left: 25px; margin-right:25px;" />
+      <p style="font-size: 11px; margin-top: 10px"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac risus ac quam cursus egestas sed in orci. Duis tempor libero nulla.  Ut eu interdum felis. Fusce quis lobortis lorem, eget consequat ligula. Etiam at porta neque. Sed quis tincidunt justo. Fusce lorem nisi.</p>
+      <div style="height: 10px;width: 10px; margin-left: 25px"/>
+      <div class="launch">
+        <img src = "./imgs/play.svg" style="margin-left: 5px; height:22px;"/>
+        <p style="margin-top: 15px; font-size: 14px; margin-left: 7px;"> LAUNCH </p>
       </div>
-    </router-link>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
-    <div class = "game"> </div>
+    </div>    
+    
+    <div class = "game4">
+      <p style = "text-align: center; padding-left: 25px;padding-top: 10px; font-size: 13px; ">
+        DISHONRED <br> 2
+      </p>
+      <div style="border-left: 1px solid #9b9b9b; height: 70px; margin-left: 25px; margin-right:25px;" />
+      <p style="font-size: 11px; margin-top: 10px"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac risus ac quam cursus egestas sed in orci. Duis tempor libero nulla.  Ut eu interdum felis. Fusce quis lobortis lorem, eget consequat ligula. Etiam at porta neque. Sed quis tincidunt justo. Fusce lorem nisi.</p>
+      <div style="height: 10px;width: 10px; margin-left: 25px"/>
+      <div class="launch">
+        <img src = "./imgs/play.svg" style="margin-left: 5px; height:22px;"/>
+        <p style="margin-top: 15px; font-size: 14px; margin-left: 7px;"> LAUNCH </p>
+      </div>
+    </div>    
+    <router-link to="/lib">
+
+    <div class = "game5">
+      <p style = "text-align: center; padding-left: 25px;padding-top: 10px; font-size: 28px;">
+        RUST
+      </p>
+      <div style="border-left: 1px solid #9b9b9b; height: 70px; margin-left: 25px; margin-right:25px;" />
+      <p style="font-size: 11px; margin-top: 10px"> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ac risus ac quam cursus egestas sed in orci. Duis tempor libero nulla.  Ut eu interdum felis. Fusce quis lobortis lorem, eget consequat ligula. Etiam at porta neque. Sed quis tincidunt justo. Fusce lorem nisi.</p>
+      <div style="height: 10px;width: 10px; margin-left: 25px"/>
+      <div class="launch">
+        <img src = "./imgs/play.svg" style="margin-left: 5px; height:22px;"/>
+        <p style="margin-top: 15px; font-size: 14px; margin-left: 7px;"> LAUNCH </p>
+      </div>
+    </div>    
+  </router-link>
+
   </div>
 </template>
 
@@ -56,12 +88,54 @@
       display: flex;
       background-color: #D9D9D9;
       border-radius: 15px;
-      height: 90px;
+      height: 110px;
       margin-top: 13px;
       align-items: center;
       align-content: center;
       transition: 0.3s;
-      background-image: url("imgs/corn.jpg");
+      background-image: url("imgs/castless.jpg");
+      background-position: 0px; /* Center the image */
+      background-size:cover;
+
+    }
+    .game3{
+      display: flex;
+      background-color: #D9D9D9;
+      border-radius: 15px;
+      height: 110px;
+      margin-top: 13px;
+      align-items: center;
+      align-content: center;
+      transition: 0.3s;
+      background-image: url("imgs/cybss.jpg");
+      background-position: 0px; /* Center the image */
+      background-size:cover;
+
+    }
+    .game4{
+      display: flex;
+      background-color: #D9D9D9;
+      border-radius: 15px;
+      height: 110px;
+      margin-top: 13px;
+      align-items: center;
+      align-content: center;
+      transition: 0.3s;
+      background-image: url("imgs/dishss.jpg");
+      background-position: 0px; /* Center the image */
+      background-size:cover;
+
+    }
+    .game5{
+      display: flex;
+      background-color: #D9D9D9;
+      border-radius: 15px;
+      height: 110px;
+      margin-top: 13px;
+      align-items: center;
+      align-content: center;
+      transition: 0.3s;
+      background-image: url("imgs/rustss.jpg");
       background-position: 0px; /* Center the image */
       background-size:cover;
 
@@ -71,14 +145,15 @@
       text-decoration: none;
     }
     .launch{
-      width: 100px;
+      width: 110px;
       display: flex;
       align-items: center;
       height: 35px;
       border-radius: 15px;
       background-color: #266DD3;
-      margin-left: auto;
+      margin-left: auto + 10px;
       padding-right: 100px;
+
 
     }
 
@@ -88,10 +163,27 @@
 
     }
     .game2:hover{
-    transform: scale(1.01);
+    transform: scale(1.005);
     cursor: pointer;
 
   }
+  .game3:hover{
+    transform: scale(1.005);
+    cursor: pointer;
+
+  }
+  .game4:hover{
+    transform: scale(1.005);
+    cursor: pointer;
+
+  }
+  .game5:hover{
+    transform: scale(1.005);
+    cursor: pointer;
+
+  }
+
+
 
 a {
   color: white;
